cfcd04291677ce2c4c4a44e6e394a51611e8f860
NeuroSys <you@example.com>
2020-01-25 15:57:39 +0200

    Example about how to use a setter with a constant (the constant is provided in the xml file)

---
+++ chapter_05_maven/src/main/java/org/neurosys/ex_04/App.java
@@ -0,0 +1,30 @@
+package org.neurosys.ex_04;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+		// Load the spring config file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext_04.xml");
+		
+		// Retrieve bean from spring container
+		ICoach theCoach = context.getBean("myCoach", ICoach.class);
+		ICoach cricketCoach = context.getBean("myCricketCoach", ICoach.class);
+
+		// call methods on the bean
+		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach.getDailyFortune());
+
+		System.out.println("=========== Cricket Coach ==========");
+		System.out.println(cricketCoach.getDailyWorkout());
+		System.out.println(cricketCoach.getDailyFortune());
+		System.out.println(((CricketCoach)cricketCoach).getEmailAddress());
+		System.out.println(((CricketCoach)cricketCoach).getTeam());
+
+		// close the context
+		context.close();
+
+	}
+
+}
---
+++ chapter_05_maven/src/main/java/org/neurosys/ex_04/TrackCoach.java
@@ -0,0 +1,18 @@
+package org.neurosys.ex_04;
+
+public class TrackCoach extends AbstractCoach implements ICoach {
+	private IFortuneService fortuneService;
+
+	public TrackCoach(IFortuneService ifs) {
+		this.fortuneService = ifs;
+	}
+
+	public String getDailyWorkout() {
+		return "Run a hard 5k";
+	}
+
+	public String getDailyFortune() {
+		return "Just do it! " + fortuneService.getFortune();
+	}
+
+}
